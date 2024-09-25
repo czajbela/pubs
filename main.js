667e1a90cd37edc2d6a37000a5ef0c140ea5db1a
czajbela <142536661+czajbela@users.noreply.github.com>
2024-09-25 08:09:41 -0300

    .

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ function soma (num1,num2){
 console.log(soma(5,2))
 console.log(soma(5,2))
 
-function areaQuadrado (num1,num2){
-    return num1*num2;
+function areaQuadrado (lado){
+    return lado**2;
 }
