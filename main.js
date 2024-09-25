00883f1bd8ad44b567a7c9214a517b2f089baac6
czajbela <142536661+czajbela@users.noreply.github.com>
2024-09-25 08:24:57 -0300

    .

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ function areaQuadrado (lado){
     return lado**2;
 }
 console.log(areaQuadrado (6))
-let media= function media(num1,num2,num3,num4){
-return num1+num2+num3/num4;
+let media= function media(num1,num2,num3){
+return num1+num2+num3/3;
 }
