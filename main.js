d39d1df1f5e05805a589398eb373e35e14c67276
czajbela <142536661+czajbela@users.noreply.github.com>
2024-10-02 07:38:45 -0300

    a

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-function soma (num1,num2){
-    return num1+num2;
-}
-console.log(soma(5,2))
-function areaQuadrado (lado){
-    return lado**2;
-}
-console.log(areaQuadrado (6));
+// function soma (num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma(5,2))
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado (6));
 
-parseInt(prompt('insira três valores'))
-let media= function media(num1,num2,num3){
-return num1+num2+num3/3;
-}
-console.log(media (1,2,3));
+// parseInt(prompt('insira três valores'))
+// let media= function media(num1,num2,num3){
+// return num1+num2+num3/3;
+// }
+// console.log(media (1,2,3));
+let estado=['PR','RJ','SP','SC','RS','ES','MG','GO','DF','MS','MT','BA','RO','AC','AM','PA','AL','MA','RO','SE','RN','PI','CE','AP','RR','TO','PB']
