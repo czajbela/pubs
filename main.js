0fc385e5a1371a2538c862a59fcb7854ff2abf01
czajbela <142536661+czajbela@users.noreply.github.com>
2024-10-02 08:15:38 -0300

    .

--- main.js
+++ main.js
@@ -19,7 +19,10 @@ while(i<estados.length){
     console.log(estados[i]);
     i=i+1;
 }
+
+
 for (let i=0; i < estados.length; i++) {
     console.log(i);
-    console.log(estados[i]);=estados[i];
-}
+    console.log(estados[i]);}
+
+
